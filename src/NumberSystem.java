7a6e5076d44312e905c5d86e6aaf5ea1163cc268
sujit1984 <sujitsugathan2002@gmail.com>
2024-09-21 23:19:40 +0530

    Basics of Java970

--- src/NumberSystem.java
+++ src/NumberSystem.java
@@ -1,3 +1,5 @@
+
+
 public class NumberSystem {
 
     public static void main(String[] args) {
@@ -5,12 +7,20 @@ public class NumberSystem {
         byte b2 = 0b1010;
         byte b3 = 012;
         byte b4 = 0XA;
-
+        int x = 5;
+        int y = -5;
+        //String s = Integer.toBinaryString(x);
+        char ch = 0x03C9;
         System.out.println(b1);
         System.out.println(b2);
         System.out.println(b3);
         System.out.println(b4);
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(y));
+        System.out.println(ch);
 
+        for (char c = 0x0900 ; c < 0x0970; c ++)
+            System.out.print(c + "  ");
 
     }
 
